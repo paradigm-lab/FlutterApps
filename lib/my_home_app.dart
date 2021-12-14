01042acf1b005febe3d0fdc9ee8c6b28fdd7ba2c
collins <collinsboniface1000@gmail.com>
2021-12-14 19:42:27 +0200

    using the none method to make the image to fit according to it's width and heigth

--- lib/my_home_app.dart
+++ lib/my_home_app.dart
@@ -27,7 +27,7 @@ class _MyHomeAppState extends State<MyHomeApp> {
           width: double.infinity,
         child: Image.asset(
         'asset/pic5.jpg',
-          fit: BoxFit.fitWidth,
+          fit: BoxFit.none,
         ),
 
 
