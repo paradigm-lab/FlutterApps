f5f10874ca8ac3eb69d3f4831b12e1e1648c68f0
collins <collinsboniface1000@gmail.com>
2021-12-15 21:43:50 +0200

    Using the repeatY method to repeat the image in the Y axis and for the X to repeat in the X axis

--- lib/my_home_app.dart
+++ lib/my_home_app.dart
@@ -27,7 +27,7 @@ class _MyHomeAppState extends State<MyHomeApp> {
           width: double.infinity,
         child: Image.asset(
         'asset/pic5.jpg',
-         repeat: ImageRepeat.repeat,
+         repeat: ImageRepeat.repeatY,
          //alignment: Alignment.bottomCenter,
           //fit: BoxFit.scaleDown,
         ),
