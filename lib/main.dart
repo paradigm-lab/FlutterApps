127b3e525021f7a2de4ee155db19fd718be7fc56
collins <collinsboniface1000@gmail.com>
2021-12-24 07:03:19 +0200

    Updating the return state for the new project

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:toast/toast.dart';
 
-import './my_home_app.dart';
-
+//import './my_home_app.dart';
+import './myProject.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: MyHomeApp()
     );
   }
+
 }
 
