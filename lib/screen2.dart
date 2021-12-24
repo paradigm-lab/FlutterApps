d6cffd8c2f0342dafc0e5184c394d1d4e621e45e
collins <collinsboniface1000@gmail.com>
2021-12-24 08:34:37 +0200

    Creating a new screen2 file for the screen2 class

---
+++ lib/screen2.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class screen2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Screen2"),
+      ),
+    );
+  }
+}
